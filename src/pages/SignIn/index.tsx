c878b74a38eb0f95c193d72f5d77c097cdca3f05
Rodrigo Luiz Duarte <rodrigo.luizduarte@gmail.com>
2021-04-04 13:59:58 -0300

    Criando toast messages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -49,9 +49,10 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (error) {
-        const errors = getValidationErrors(error);
-
-        formRef.current?.setErrors(errors);
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn]
